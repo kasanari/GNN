578c6295b09f431ac67a5d0c6e37aac97d425e54
Jakob <jaknyb@kth.se>
2024-05-08 16:11:27 +0200

    fix import

--- gnn_policy/__init__.py
+++ gnn_policy/__init__.py
@@ -1,9 +1,5 @@
-from .graph_net import MultiMessagePassing
-from .graph_net_geometric import TGMessagePassing
-from .graph_net_local import LocalMultiMessagePassing
+from .graph_policy import GNNPolicy
 
 __all__ = [
-	"MultiMessagePassing",
-	"TGMessagePassing",
-	"LocalMultiMessagePassing",
+	"GNNPolicy",
 ]
